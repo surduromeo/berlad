2d009326025edb4032a871443f06d4fbd39b7da7
surduromeo <46320239+surduromeo@users.noreply.github.com>
2026-03-08 08:12:22 +0200

    Rename 'csb' to 'stok' in informatiiPoza.js

--- src/informatiiPoza.js
+++ src/informatiiPoza.js
@@ -81,7 +81,7 @@ var informatiiPoza = {
 	
 		
 	
-	'csb': {
+	'stok': {
         titlu: 'Complexul Şcolar Bârlad',
         descriere: '<p align="justify">La începutul anului 1958, printr-o Hotărâre a Consiliului de Miniştri, ansamblul de clădiri construit în oraşul Bârlad cu destinaţia Liceu militar sau Academie militară a fost transferat de la Ministerul Forţelor Armate la Ministerul Învăţământului, care a decis înfiinţarea unui Complex Şcolar, în care să fie instruiţi aproape toţi elevii din oraş. Lucrările au fost finalizate în noiembrie 1958 şi tot atunci Inspectoratul Şcolar al Regiunii Iaşi a hotărât mutarea în noul local a tuturor elevilor din ciclul gimnazial şi liceal. Astfel, elevii din trei licee bârlădene (Liceul „Gh. Roşca-Codreanu”, Liceul de fete, Liceul Pedagogic) şi şase şcoli generale au devenit elevii noii instituţii de învăţământ care, printr-un ordin al Ministerului, a fost numit „Complexul Şcolar Bârlad”.</p>\
 		<p align="justify">www.liceuleminescubarlad.ro\</p>'
